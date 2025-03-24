cedc833876b5e9ffec63ab8495d7fdff91622d2f
Pepel <lera.kalashnikova2018@gmail.com>
2025-03-24 19:39:55 +0200

    fix

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -8,7 +8,6 @@ import {
   refreshUserSessionController,
   registerUserController,
 } from '../controllers/auth.js';
-import { authenticate } from '../middlewares/authenticate.js';
 
 const router = Router();
 
@@ -24,12 +23,8 @@ router.post(
   ctrlWrapper(loginUserController),
 );
 
-router.post(
-  '/refresh',
-  authenticate,
-  ctrlWrapper(refreshUserSessionController),
-);
+router.post('/refresh', ctrlWrapper(refreshUserSessionController));
 
-router.post('/logout', authenticate, ctrlWrapper(logoutUserController));
+router.post('/logout', ctrlWrapper(logoutUserController));
 
 export default router;
