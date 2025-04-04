a680b69d58b590770f46dfb95d1c915cf19d159f
Pepelok <lera.kalashnikova2018@gmail.com>
2025-04-04 16:21:13 +0300

    bacl

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,17 +1,4 @@
 import path from 'path';
 
-export const SORT_ORDER = {
-    ASC: 'asc',
-    DESC: 'desc',
-  };
-
 export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'src', 'tmp');
 export const UPLOAD_DIR = path.join(process.cwd(), 'src', 'uploads');
-
-export const SMTP = {
-  SMTP_HOST: 'SMTP_HOST',
-  SMTP_PORT: 'SMTP_PORT',
-  SMTP_USER: 'SMTP_USER',
-  SMTP_PASSWORD: 'SMTP_PASSWORD',
-  SMTP_FROM: 'SMTP_FROM',
-};
